b474f5c483e19648dd1a6c90c66f7cda6205f340
harrybin <harry.binnendijk@gmail.com>
2013-04-09 23:29:43 +1000

    add get-company by id

--- src/linkedin/api.clj
+++ src/linkedin/api.clj
@@ -1,6 +1,7 @@
 (ns linkedin.api
   (:require [linkedin.core :as lc]
-            [ring.util.codec :as ruc])
+            [ring.util.codec :as ruc]
+            [clojure.tools.logging :refer [debug info]])
   )
 
 (def field-selector "(id,headline,first-name,last-name,picture-url)")
@@ -14,8 +15,12 @@
   (lc/repeated (str "http://api.linkedin.com/v1/people-search:(people:" field-selector ")?keywords=" keyword)
             access-token))
 
+(def company-fields "id,name,industries,employee-count-range,locations:(address:(city,country-code))")
+
 (defn search-companies [access-token name]
-  (let [fields "id,name,industries,employee-count-range,locations:(address:(city,country-code))"
-        name (ruc/url-encode name)]
-    (lc/repeated (str "http://api.linkedin.com/v1/company-search:(companies:(" fields
+  (let [name (ruc/url-encode name)]
+    (lc/repeated (str "http://api.linkedin.com/v1/company-search:(companies:(" company-fields
                       "))?keywords={" name "}") access-token :max-repeats 1)))
+
+(defn get-company [access-token id]
+  (lc/get-url (str "http://api.linkedin.com/v1/companies/" id ":(" company-fields ")") access-token))
